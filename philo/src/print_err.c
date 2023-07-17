6611d8e5cfa30333b57d8edff5d22139905c122a
Luis <luissoto2094@gmail.com>
2023-07-17 17:17:52 +0200

    print?err modified

--- philo/src/print_err.c
+++ philo/src/print_err.c
@@ -7,7 +7,7 @@ void    print_err(int err)
     if (err == INVALID_NUMBER)
         printf("Invalid number of arguments. See subject.\n");
     else if (err == INVALID_CONTENT)
-        printf("One of your arguments has invalid content, \
-check subject again.\n");
+        printf("One of your arguments is invalid, \
+check subject and try again.\n");
     exit (1);
 }
