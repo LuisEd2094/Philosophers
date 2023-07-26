96e30e729905611fd522c651523e40d2dfcf0a3d
Luis <luissoto2094@gmail.com>
2023-07-26 12:03:13 +0200

    is working once again for one philo

--- philo/src/Program/one_philo_case.c
+++ philo/src/Program/one_philo_case.c
@@ -4,7 +4,8 @@ void    one_philo_case(t_prg *prg)
 {
     prg->start_time = get_time(prg);
     prg->f_test = &routine;
-    create_thread(&(prg->tid[0]), &(routine), &(prg->philos[0]));
+    if (!create_thread(&(prg->tid[0]), &(routine), &(prg->philos[0])))
+        close_prg(prg);
     pthread_detach(prg->tid[0]);
     while(check_conditions_continue_thread(prg))
     {
--- philo/src/Program/print_philo_state.c
+++ philo/src/Program/print_philo_state.c
@@ -5,6 +5,6 @@ void    print_philo_state(char *str, t_philo *philo)
 {
     uint64_t    time;
 
-    time = get_time(philo->prg) - philo->prg->start_time;
+    time = get_time(philo->prg) - (philo->prg->start_time);
     printf("%lu %d %s\n", time, philo->id, str);
 }
